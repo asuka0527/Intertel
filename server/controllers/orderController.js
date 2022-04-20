fbc3db8f20eca528f393a92517696bbbd3901862
Asuka Nakajima <junizzanakajima@gmail.com>
2022-04-20 11:19:31 +0900

    fixed order controller and order model

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,43 +6,44 @@ const asyncHandler = require('express-async-handler')
 // @description    Create new order
 // @route          POST/api/order
 // @access         Private
-createOrder = (req, res) => {
+createOrder = asyncHandler(async(req, res) => {
     const body = req.body
 
-    if (!body) {
+    if (!body.customerName) {
         return res.status(400).json({
             success: false,
-            error: 'You must provide a order',
+            message: 'You must provide a Customer Name',
         })
     }
 
-    const order = new Order(body)
+    const order = await Order.create({
+        customerName  :body.customerName,
+        customerPhone : body.customerPhone,
+        customerAddress : body.customerAddress,
+        agentName : body.agentName,
+        agentPhone : body.agentPhone,
+        agentAddress : body.agentAddress,
+        quantity : body.quantity,
+        modelName : body.modelName,
+        serialNumber : body.serialNumber,
+        mac1 : body.mac1,
+        mac2 : body.mac2,
+        boxNo : body.boxNo,
+        sealNo : body.sealNo,
+        soldAt : body.soldAt,
+        invoiceAt : body.invoiceAt,
+        shippedAt : body.shippedAt,
+        returnedAt : body.returnedAt,
+        note : body.note
+      })
 
-    if (!order) {
-        return res.status(400).json({ success: false, error: err })
-    }
-
-    order
-        .save()
-        .then(() => {
-            return res.status(201).json({
-                success: true,
-                id: order._id,
-                message: 'Order created!',
-            })
-        })
-        .catch(error => {
-            return res.status(400).json({
-                error,
-                message: 'Order not created!',
-            })
-        })
-}
+      res.status(200).json(order)
+})
 
 // @description     Update order
 // @route           GET/api/orders/:id
 // @access          Private
-updateOrder = async (req, res) => {
+updateOrder = asyncHandler(async (req, res) => {
     const body = req.body
 
     if (!body) {
@@ -52,61 +53,63 @@ updateOrder = async (req, res) => {
         })
     }
 
-    Order.findOne({ _id: req.params.id }, (err, order) => {
-        if (err) {
-            return res.status(404).json({
-                err,
-                message: 'order not found!',
-            })
-        }
-        order.name = body.name
-        order.time = body.time
-        order.rating = body.rating
-        order
-            .save()
-            .then(() => {
-                return res.status(200).json({
-                    success: true,
-                    id: order._id,
-                    message: 'Order updated!',
-                })
-            })
-            .catch(error => {
-                return res.status(404).json({
-                    error,
-                    message: 'Order not updated!',
-                })
-            })
-    })
-}
+  const order = await Order.findById(req.params.id);
+
+   //if the order found update these properties
+   if (order) {
+       
+    order.customerName = body.customerName
+    order.customerPhone = body.customerPhone
+    order.customerAddress = body.customerAddress
+    order.agentName = body.agentName
+    order.agentPhone = body.agentPhone
+    order.agentAddress = body.agentAddress
+    order.quantity = body.quantity
+    order.modelName = body.modelName
+    order.serialNumber = body.serialNumber
+    order.mac1 = body.mac1
+    order.mac2 = body.mac2
+    order.boxNo = body.boxNo
+    order.sealNo = body.sealNo
+    order.soldAt = body.soldAt
+    order.invoiceAt = body.invoiceAt
+    order.shippedAt = body.shippedAt
+    order.returnedAt = body.returnedAt
+    order.note = body.note
+
+    // save the updated order to db return it to the body
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+
+})
 
 // @description     Delete order
 // @route           GET/api/orders/:id
 // @access          Private
-deleteOrder = async (req, res) => {
-    await Order.findOneAndDelete({ _id: req.params.id }, (err, order) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+deleteOrder = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (order) {
+      await order.remove();
+      res.json({ message: "Order removed" });
+    } else {
+      res.status(404);
+      throw new Error("Order not found");
+    }
+  });
+  
 
-        if (!order) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Order not found` })
-        }
-
-        return res.status(200).json({ success: true, data: order })
-    }).catch(err => console.log(err))
-}
 
 // @description    Get order by ID
 // @route          GET/api/orders/:id
 // @access         Private
-getOrderById = async (req, res) => {
+getOrderById = asyncHandler(async (req, res) => {
     
     const order = await Order.findOne({ _id: req.params.id }).populate("dispositions");
-
-    console.log(order.disposition);
 
     if (order) {
         res.json(order);
@@ -114,24 +117,39 @@ getOrderById = async (req, res) => {
         res.status(404);
         throw new Error("Order not found");
       }
-}
+})
 
 // @description    Get all orders
 // @route          GET/api/orders
 // @access         Private
-getOrders = async (req, res) => {
-    await Order.find({}, (err, orders) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
+getOrders = asyncHandler(async (req, res) => {
+    const pageSize = 3;
+    const page = Number(req.query.page) || 1;
+
+    const keyword = req.query.keyword
+      ? {
+          customerName: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
         }
-        if (!orders.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Orders not found` })
-        }
-        return res.status(200).json({ success: true, data: orders })
-    }).populate("dispositions").catch(err => console.log(err))
-}
+      : {};
+
+  const count = await Order.countDocuments({ ...keyword });
+
+  const orders = await Order.find({ ...keyword })
+  .populate("dispositions")
+  .limit(pageSize)
+  .skip(pageSize * (page - 1));
+
+  if(orders.length > 0){
+      
+      res.json({ orders, page, pages: Math.ceil(count / pageSize) });
+    }
+    else {
+        res.status(404)
+    }
+})
 
 // @description    Add disposition to order
 // @route          POST/api/orders/:id/disposition
@@ -145,7 +163,7 @@ addDisposition = asyncHandler(async (req, res) => {
         
     } = req.body
 
-    const foundOrder = await Order.findById(req.params.id);
+    const foundOrder = await Order.findById(req.params.id).populate("dispositions");
     
     if(foundOrder){
         const disposition = new Disposition({
@@ -167,7 +185,17 @@ addDisposition = asyncHandler(async (req, res) => {
     }
 })
 
-// @description    Delete disposition to order
+// @description    Update disposition in order
+// @route          PUT/api/orders/:id/disposition/:dispositionId
+// @access         Private
+
+updateDisposition = asyncHandler (async(req,res) => {
+    const {id, dispositionId } = req.params;
+
+    // await Disposition.findByIdAndUpdate(id, )
+})
+
+// @description    Delete disposition from order
 // @route          DELETE/api/orders/:id/disposition/:dispositionId
 // @access         Private
 removeDisposition = asyncHandler(async (req, res) => {
@@ -187,5 +215,6 @@ module.exports = {
     getOrders,
     getOrderById,
     addDisposition,
+    updateDisposition,
     removeDisposition
 }
--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -6,12 +6,24 @@ const Item = require('../models/itemModel');
 
 const OrderSchema = new Schema(
     {
+        customerName: { type: String, required: true },
+        customerPhone: { type: String, required: false },
+        customerAddress: { type: String, required: false },
+        agentName: { type: String, required: true },
+        agentPhone: { type: String, required: false },
+        agentAddress: { type: String, required: false },
+        quantity: {   type: Number,required: true,default: 0},
         modelName: { type: String, required: true },
         serialNumber: { type: String, required: true },
         mac1: { type: String, required: false },
         mac2: { type: String, required: false },
         boxNo: { type: String, required: false },
         sealNo: { type: String, required: false },
+        soldAt: { type: Date },
+        invoiceAt: { type: Date },
+        shippedAt: { type: Date },
+        returnedAt: { type: Date },
+        note: { type: String, required: false },
         dispositions: [{type: mongoose.Schema.Types.ObjectId, ref:'disposition'}],
         time: { type: [String], required: true }
     },
